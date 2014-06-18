d574f046ecbe64caf9337192e386480691278189
AShabana <AhmedShabana2000@gmail.com>
2014-06-18 18:32:54 +0200

    Update Auth.php

--- MonitorInterface/Auth.php
+++ MonitorInterface/Auth.php
@@ -3,6 +3,7 @@
 	if(empty($_SESSION['sessionid'])){
 		include 'auth_form.html' ;
 		if(  isset($_POST["Fname"]) && isset($_POST["Lname"]) && $_POST["Lname"] != ""){
+			 $config = parse_ini_file("config.ini",true) ;
 			if ( function_exists("ldap_connect"){
 			    $ldap = ldap_connect($config["ldap"]["host"]);
 			    ldap_bind($ldap, $_POST['Fname'], $_POST['Lname']);
@@ -14,11 +15,11 @@
 			}
 			elseif( $_POST["Fname"] == $config["glabal"]["admin"] &&  $config["global"]["admin_password"] == $_POST['Lname']  ) { 
 				print "Sucesss ..<br/>";
-					session_start();
-					$sid = session_id() ;
-			    setcookie("CCS_MON_IF_COOKIE",$sid, time()+3600*12);
-			    $_SESSION['sessionid'] = session_id();
-		    	header( 'Location:index.php');
+				session_start();
+				$sid = session_id() ;
+				setcookie("CCS_MON_IF_COOKIE",$sid, time()+3600*12);
+				$_SESSION['sessionid'] = session_id();
+		    		header( 'Location:index.php');
 		 } 
 		 else {
 	        	print "Login failed ..";
