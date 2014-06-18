cce3327e91bdb60eb4ecc8a1047959403ac1113e
AShabana <AhmedShabana2000@gmail.com>
2014-06-18 20:16:50 +0200

    Update getkannels.php

--- MonitorInterface/getkannels.php
+++ MonitorInterface/getkannels.php
@@ -18,13 +18,14 @@
                 $pass = exec("grep '^status-password' $Conf[$i] | cut -d'=' -f2 | sed 's/ //g'");
                 print  $i+1  . ")) $Conf[$i] [ <a href=http://" . $appConf['global']['portal_host'] . $appConf['global']['portal_port'] . "/get_kanStatus.php?pass=$pass&port=$port> Status </a>]
                                              [ <a href=http://" . $appConf['global']['portal_host'] . $appConf['global']['portal_port'] . "/get_kanStore.php?pass=$pass&port=$port> Store </a>]
-                                             [ <a href=http://" . $appConf['global']['portal_host'] . $appConf['global']['portal_port'] . "/get_kanStatus_v2.php?pass=$pass&port=$port> Contole links </a>]
-                                             [ <a href=http://" . $appConf['global']['portal_host'] . $appConf['global']['portal_port'] . "/_get_kanAggregated.php?pass=$pass&port=$port> Aggregated by status snapshot </a>]
-                                             [ <a href=http://" . $appConf['global']['portal_host'] . $appConf['global']['portal_port'] . "/lookup.php> Look inside BB current access log </a>]</br>" ;
-
+                                             [ <a href=http://" . $appConf['global']['portal_host'] . $appConf['global']['portal_port'] . "/control_smscs.php?pass=$pass&port=$port> Contole links </a>]
+                                             [ <a href=http://" . $appConf['global']['portal_host'] . $appConf['global']['portal_port'] . "/get_kanAggregated.php?pass=$pass&port=$port> Aggregated by status snapshot </a>]
+                                             [ <a href=http://" . $appConf['global']['portal_host'] . $appConf['global']['portal_port'] . "/lookup.php> Look inside BB current access log"
+                </br>" ;
+                
 
         }
-
-
+        
+        
 ?>
 <li><a href="index.php">Main Page</a> Return to your home</li>
