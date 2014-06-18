5cb025b0bac3a6e8f538dba0a2d28bb1faaa3d86
AShabana <AhmedShabana2000@gmail.com>
2014-06-18 19:03:25 +0200

    Update get_kanStatus.php

--- MonitorInterface/get_kanStatus.php
+++ MonitorInterface/get_kanStatus.php
@@ -1,14 +1,15 @@
 <?php
 
             session_start();
-        if( empty($_SESSION['sessionid']) || !isset($_COOKIE["CCS_MON_IF_COOKIE"])){
-                require_once  "Auth.php"  ;exit(11);
-        }
+            if( empty($_SESSION['sessionid']) || !isset($_COOKIE["CCS_MON_IF_COOKIE"])){
+                        require_once  "Auth.php"  ;
+                        exit(11);
+            }
 
 
-                        $url = "http://localhost:" . $_GET["port"] . "/status?password=" . $_GET["pass"];
-                        $output = file_get_contents($url) ;
-                        foreach  (explode("\n",$output) as $i){
-                                echo   $i . "</br>"  ;
-                        }
+            $url = "http://localhost:" . $_GET["port"] . "/status?password=" . $_GET["pass"];
+            $output = file_get_contents($url) ;
+            foreach  (explode("\n",$output) as $i){
+                        echo   $i . "</br>"  ;
+            }
 ?>
